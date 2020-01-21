22edffdcd66134ee46d0abe5d254ebb87041c16e
fonkamloic <fonkamloic@gmail.com>
2020-01-21 21:03:00 +0100

    first commit

---
+++ android/app/src/main/kotlin/me/fonkamloic/mtn_api_calls/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.fonkamloic.mtn_api_calls
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/momo_constants.dart
@@ -0,0 +1,22 @@
+const URL_SAND_PROVISION = "https://sandbox.momodeveloper.mtn.com/v1_0";
+
+String APIUSER_COLLECTION = "95d497e4-d8b2-4676-bfa2-60cb15d0c8ff";
+String APIKEY_COLLECTION = "0f832fbf454c4d5f8428e954e9b5bf8c";
+
+String APIUSER_DISBURSEMENT = "11b934ec-b61d-4ad0-aa26-c690e2e20679";
+String APIKEY_DISBURSEMENT = "4bba4d163e4b495eb5dd70f25e411b08";
+
+const PRIMARY_KEY_COLLECTION = "0bd82098b7ec4354a796ea6c1f0c95f8";
+const PRIMARY_KEY_DISBURSE = "84b86f835e1245e4b302af4385bc5f27";
+
+const TARGET_ENV = "sandbox";
+const CALLBACK_HOST = "https://c0llegeplan.firebaseapp.com";
+
+const URL_COLLECTION_TOKEN = "https://sandbox.momodeveloper.mtn.com/collection";
+const URL_DISBURSEMENT_TOKEN =
+    "https://sandbox.momodeveloper.mtn.com/disbursement/token/";
+
+const URL_COLLECTION = "https://sandbox.momodeveloper.mtn.com/collection/v1_0";
+const URL_DISBURSEMENT =
+    "https://sandbox.momodeveloper.mtn.com/disbursement/v1_0";
+
